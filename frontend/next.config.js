547219c1fa82e1adbdf33ad08b9a3a3a6bf324cb
aaryanmittal154 <aaryanmittal154@gmail.com>
2025-03-03 11:24:33 -0800

    made changes for deployment frontedn

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NEXT_PUBLIC_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
+    NEXT_PUBLIC_OAUTH_CLIENT_ID: process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID || "",
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || ""}/api/:path*`,
       },
     ];
   },
